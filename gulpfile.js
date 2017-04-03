f9b3a98e1fec58dd7bfea5fcbc9e8d9b93253239
Josef Kolář <thejoeejoee@gmail.com>
2017-04-03 17:17:41 +0200

    Added template

---
+++ gulpfile.js
@@ -0,0 +1,113 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var browserSync = require('browser-sync').create();
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var deploy = require('gulp-gh-pages');
+
+
+// Compile LESS files from /less into /css
+gulp.task('less', function () {
+    return gulp.src('less/creative.less')
+        .pipe(less())
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Minify compiled CSS
+gulp.task('minify-css', ['less'], function () {
+    return gulp.src('css/creative.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Minify JS
+gulp.task('minify-js', function () {
+    return gulp.src('js/creative.js')
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Copy vendor libraries from /node_modules into /vendor
+gulp.task('copy', function () {
+    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
+        .pipe(gulp.dest('vendor/bootstrap'));
+
+    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+        .pipe(gulp.dest('vendor/jquery'));
+
+    gulp.src(['node_modules/magnific-popup/dist/*'])
+        .pipe(gulp.dest('vendor/magnific-popup'));
+
+    gulp.src(['node_modules/scrollreveal/dist/*.js'])
+        .pipe(gulp.dest('vendor/scrollreveal'));
+
+    gulp.src([
+        'node_modules/font-awesome/**',
+        '!node_modules/font-awesome/**/*.map',
+        '!node_modules/font-awesome/.npmignore',
+        '!node_modules/font-awesome/*.txt',
+        '!node_modules/font-awesome/*.md',
+        '!node_modules/font-awesome/*.json'
+    ])
+        .pipe(gulp.dest('vendor/font-awesome'))
+});
+
+// Copy all need to deploy to /dist folder
+gulp.task('build', function () {
+    gulp.src([
+        'vendor/**/*'
+    ]).pipe(gulp.dest('dist/vendor'));
+    gulp.src([
+        'index.html'
+    ]).pipe(gulp.dest('dist'));
+    gulp.src([
+        'js/creative.min.js'
+    ]).pipe(gulp.dest('dist/js'));
+    gulp.src([
+        'js/creative.min.css'
+    ]).pipe(gulp.dest('dist/css'));
+    gulp.src([
+        'img/**/*'
+    ]).pipe(gulp.dest('dist/img'));
+
+});
+
+// Run everything
+gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: ''
+        }
+    })
+});
+
+gulp.task('deploy', function () {
+    return gulp.src("./dist/**/*")
+        .pipe(deploy())
+});
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function () {
+    gulp.watch('less/*.less', ['less']);
+    gulp.watch('css/*.css', ['minify-css']);
+    gulp.watch('js/*.js', ['minify-js']);
+    // Reloads the browser whenever HTML or JS files change
+    gulp.watch('*.html', browserSync.reload);
+    gulp.watch('js/**/*.js', browserSync.reload);
+});
